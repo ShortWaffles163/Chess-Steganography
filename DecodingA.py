c0dc9763103c0596022eb12a4599a571fae5c28e
ShortWaffles <wafi.khan.2013@gmail.com>
2026-08-12 19:28:45 +1000

    Update DecodingA.py

--- DecodingA.py
+++ DecodingA.py
@@ -1,86 +1,84 @@
 from time import time
-from math import log2
 from chess import pgn, Board
-from util import to_binary_string, get_pgn_games
+import io
 
+def get_pgn_games(pgn_string: str):
+    """Robustly extract all games from a PGN string."""
+    games = []
+    pgn_io = io.StringIO(pgn_string)
+    while True:
+        game = pgn.read_game(pgn_io)
+        if game is None:
+            break
+        games.append(game)
+    return games
 
-###
-### Pass in a PGN string of 1 or more games
-### and also the file path that it should write to in the end
-###
 def decode(pgn_string: str, output_file_path: str):
     start_time = time()
+    
+    games = get_pgn_games(pgn_string)
+    
+    if not games:
+        print("Error: No valid games found in PGN string.")
+        return b""
 
-    total_move_count = 0
+    with open(output_file_path, "wb") as output_file:
+        output_data = ""
+        total_move_count = 0
+        FIXED_CHUNK_SIZE = 4
+        
+        for game in games:
+            chess_board = Board()
+            game_moves = list(game.mainline_moves())
+            total_move_count += len(game_moves)
+            
+            for move in game_moves:
+                legal_move_ucis = [
+                    m.uci() for m in sorted(
+                        list(chess_board.generate_legal_moves()), 
+                        key=lambda x: x.uci()
+                    )
+                ]
+                
+                if len(legal_move_ucis) < (2 ** FIXED_CHUNK_SIZE):
+                    chess_board.push_uci(move.uci())
+                    continue
+                
+                try:
+                    move_index_val = legal_move_ucis.index(move.uci())
+                except ValueError:
+                    chess_board.push_uci(move.uci())
+                    continue 
 
-    # load games from pgn file
-    games: list[pgn.Game] = get_pgn_games(pgn_string)
+                move_binary = bin(move_index_val)[2:]
+                required_padding = max(0, FIXED_CHUNK_SIZE - len(move_binary))
+                move_binary = ("0" * required_padding) + move_binary
+                
+                chess_board.push_uci(move.uci())
+                output_data += move_binary
+                
+                if len(output_data) >= 8:
+                    full_bytes = len(output_data) // 8
+                    output_file.write(
+                        bytes([int(output_data[i*8 : i*8+8], 2) for i in range(full_bytes)])
+                    )
+                    output_data = output_data[full_bytes * 8:]
+        
+        if output_data:
+            output_data = output_data.ljust(8, '0')
+            output_file.write(bytes([int(output_data, 2)]))
+    
+    print(f"\n[Decoder A] Successfully decoded: {len(games)} game(s), {total_move_count} moves ({round(time() - start_time, 3)}s).")
 
-    # convert moves to binary and write to output file
-    with open(output_file_path, "w") as output_file:
-        output_file.write("")
+    with open(output_file_path, "rb") as f:
+        result = f.read()
+    
+    # Print to Shell
+    try:
+        print(f"[Decoder A] Decoded Message: {result.decode('utf-8')}")
+    except:
+        print(f"[Decoder A] Decoded Bytes: {result}")
+    
+    return result
 
-    output_file = open(output_file_path, "ab")
-    output_data = ""
-
-    for game_index, game in enumerate(games):
-        chess_board = Board()
-
-        game_moves = list(game.mainline_moves())
-        total_move_count += len(game_moves)
-
-        for move_index, move in enumerate(game_moves):
-            # MINIMAL FIX: Added sorted() here to match the encoder
-            legal_move_ucis = [
-                legal_move.uci()
-                for legal_move in sorted(list(chess_board.generate_legal_moves()), key=lambda m: m.uci())
-            ]
-
-            # get binary of the move played, using its index in the legal moves
-            move_binary = bin(
-                legal_move_ucis.index(move.uci())
-            )[2:]
-
-            # if this is the last move of the last game,
-            # binary cannot go over a total length multiple of 8
-            if (
-                game_index == len(games) - 1 
-                and move_index == len(game_moves) - 1
-            ):
-                max_binary_length = min(
-                    int(log2(
-                        len(legal_move_ucis)
-                    )),
-                    8 - (len(output_data) % 8)
-                )
-            else:
-                max_binary_length = int(log2(
-                    len(legal_move_ucis)
-                ))
-
-            # Pad move binary to meet max binary length
-            required_padding = max(0, max_binary_length - len(move_binary))
-            move_binary = ("0" * required_padding) + move_binary
-
-            # play move on board
-            chess_board.push_uci(move.uci())
-
-            # add move binary to output data string
-            output_data += move_binary
-
-            # if output binary pool is multiple of 8, flush it to file
-            if len(output_data) % 8 == 0:
-                output_file.write(
-                    bytes([
-                        int(output_data[i * 8 : i * 8 + 8], 2)
-                        for i in range(len(output_data) // 8)
-                    ])
-                )
-                output_data = ""
-
-    print(
-        "\nsuccessfully decoded pgn with "
-        + f"{len(games)} game(s), {total_move_count} total move(s)"
-        + f"({round(time() - start_time, 3)}s)."
-    )
     
